5e78648335736c801a25d6a264d3b6a9cbc2d86f
Ravichandran2004 <dr.ravichandran2004@gmail.com>
2025-06-01 16:13:06 +0530

    Initial commit

---
+++ server/models/Admin.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["Admin", "SuperAdmin"], default: "Admin" },
+    profileImage: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export default mongoose.model("Admin", adminSchema);
